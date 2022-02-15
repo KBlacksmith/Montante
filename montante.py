3e27cc50d2d0f17889ec6eeae36143c35be16bc9
KennethRdz <78935468+KBlacksmith@users.noreply.github.com>
2022-02-15 15:09:18 -0600

    Commit inicial

---
+++ montante.py
@@ -0,0 +1,45 @@
+#Funciones o Métodos
+
+#Validar que regrese un entero positivo
+def numVariables()->int: 
+    #return num
+    pass
+
+# Preguntar al usuario si desea seguir en el programa o salir
+def continuar(): 
+    # Puede regresar una cadena o un booleano
+    pass
+
+def imprimirMatriz(matriz: list): 
+    # Solamente imprime la matriz, dandole formato para que sea legible
+    pass
+
+def imprimirMatrizYAdjunta(matriz: list, adj: list): 
+    # Similar a la anterior, pero ahora da formato para imprimir una matriz y su matriz adjunta
+    pass
+
+# Generar la matriz correspondiente al problema, preguntando por los coeficientes
+#Generar la matriz identidad asociada a la matriz del problema
+def inicializar(num: int)->tuple:
+    #Python puede regresar una tupla que puede ser "desempacada" al recibirla 
+    #return (matriz, identidad)
+    pass
+
+# Método principal
+# Decidir si será un método recursivo o solamente iterativo
+# Revisar excepciones. 
+def montante(matriz: list, adj: list): 
+    pass
+
+
+
+if __name__=="__main__": 
+    #Llamada al programa, lo equivalente a la función main() en otros lenguajes
+    # 1 - Inicio
+    # 2 - Inicializar variables
+    # 3 - Mientras sn == "s"
+        # 3.1 - num = numVariables()
+        # 3.3 - matriz, identidad = inicializar(num)
+        # 3.3 - montante(num, matriz, identidad)
+    # 4 - Fin
+    pass
